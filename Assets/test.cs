b2c486f19d969fdc55d0891c32e95b2bfaa5f20e
Katarsis <lokkins334@gmail.com>
2024-04-24 21:31:45 +0500

    init da

---
+++ Assets/test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 100;
+
+        for (; a > 0; a--)
+        {
+        }
+        Debug.Log(a);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
